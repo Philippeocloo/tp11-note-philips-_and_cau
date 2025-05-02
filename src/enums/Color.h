0bc4a7d5c4776ce57174d7f9969bacc360c4bf66
simbadLM <quentin.balloy.pro@gmail.com>
2025-05-02 12:11:11 +0200

    Ajout des enums

---
+++ src/enums/Color.h
@@ -0,0 +1,9 @@
+#pragma once
+
+enum class Color {
+    Red       = 0,
+    Blue      = 1,
+    Yellow    = 2,
+    Green     = 3,
+    Multicolor = 4
+};
---
+++ src/enums/Shape.h
@@ -0,0 +1,7 @@
+#pragma once
+
+enum class Shape {
+    Cross  = 0,
+    Circle = 1,
+    Square = 2
+};
